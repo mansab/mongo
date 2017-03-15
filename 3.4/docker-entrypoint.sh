78ea7ce7d214ee519f4fd01b93dc744f080e5448
Mansab Uppal <muppal@thoughtworks.com>
2017-03-15 11:17:21 +0100

    Change VOLUME to /data
    * /data/db & /data/configdb gets wipedout if you mount /data as NFS volume

--- 3.4/docker-entrypoint.sh
+++ 3.4/docker-entrypoint.sh
@@ -9,8 +9,11 @@ fi
 # all mongo* commands should be dropped to the correct user
 if [[ "$1" == mongo* ]] && [ "$(id -u)" = '0' ]; then
 	if [ "$1" = 'mongod' ]; then
-		chown -R mongodb /data/configdb /data/db
-	fi
+    if [ ! -d /data/db ] || [ ! -d /data/configdb ]; then
+      exec gosu mongodb "$BASH_SOURCE" "mkdir -p /data/db /data/configdb"
+      exec gosu mongodb "$BASH_SOURCE" "chmod -R 777 /data/db /data/configdb"
+	  fi
+  fi
 	exec gosu mongodb "$BASH_SOURCE" "$@"
 fi
 
